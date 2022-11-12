99817cc3edbec46f62fd0581700ce4cdfc519ecf
web3sdkio <demo@web3sdks.com>
2022-11-12 16:30:05 +0800

    First submit

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import { ConnectWallet } from "@web3sdkio/react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://web3sdk.io/">web3sdkio</a>!
+        </h1>
+
+        <p className={styles.description}>
+          Get started by configuring your desired network in{" "}
+          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
+          <code className={styles.code}>pages/index.js</code> file!
+        </p>
+
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+
+        <div className={styles.grid}>
+          <a href="https://portal.web3sdk.io/" className={styles.card}>
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              web3sdkio.
+            </p>
+          </a>
+
+          <a href="https://web3sdk.io/dashboard" className={styles.card}>
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a
+            href="https://portal.web3sdk.io/templates"
+            className={styles.card}
+          >
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing web3sdkio features.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
